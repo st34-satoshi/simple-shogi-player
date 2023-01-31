55aa67740aade9590940ce86df92165379f95ccc
Satoshi Tanaka <st34.satoshi@gmail.com>
2023-01-31 15:19:32 +0900

    remove MembershipLocationMark

--- components/Membership/MembershipLocation.vue
+++ components/Membership/MembershipLocation.vue
@@ -1,21 +1,16 @@
 <template lang="pug">
 .MembershipLocation
-  MembershipLocationMark(:location="location")
   .PlayerName(v-html="player_name" v-if="player_name")
 </template>
 
 <script>
 import { support } from "../support.js"
-import MembershipLocationMark from "./MembershipLocationMark.vue"
 
 export default {
   name: "MembershipLocation",
   mixins: [support],
   props: {
     location: { required: true },
-  },
-  components: {
-    MembershipLocationMark,
   },
   methods: {
     player_attr_of(key) {
